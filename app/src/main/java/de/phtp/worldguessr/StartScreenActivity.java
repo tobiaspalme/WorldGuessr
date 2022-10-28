6608116e1a5fdbfeb2d2ea6270e95fec242f0f87
Tobias <germanturboswitch@gmail.com>
2022-10-28 21:05:20 +0200

    implemented activities + startScreen Buttons with no logic

---
+++ app/src/main/java/de/phtp/worldguessr/StartScreenActivity.java
@@ -0,0 +1,16 @@
+package de.phtp.worldguessr;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+
+    }
+}
