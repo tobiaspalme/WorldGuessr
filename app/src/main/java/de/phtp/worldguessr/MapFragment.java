d9a896b2ef81f0748437eb037282391a9ca76521
GermanTurbo <tobiaspalme2003@gmail.com>
2022-11-02 19:57:23 +0100

    added osm Map

--- app/src/main/java/de/phtp/worldguessr/MapFragment.java
+++ app/src/main/java/de/phtp/worldguessr/MapFragment.java
@@ -1,6 +1,9 @@
 package de.phtp.worldguessr;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +13,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.Marker;
+
 import de.phtp.worldguessr.databinding.FragmentMapBinding;
 import de.phtp.worldguessr.databinding.FragmentPictureBinding;
 
@@ -17,6 +29,9 @@ public class MapFragment extends Fragment {
 
     private FragmentMapBinding binding;
     private View root;
+
+    private MapView map = null;
+
 
     @Nullable
     @Override
@@ -26,7 +41,57 @@ public class MapFragment extends Fragment {
         Log.d("M1", "MapFragment Created");
 
 
+        Context ctx = getActivity();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+
+        map = binding.map;
+
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(9.5);
+        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        mapController.setCenter(startPoint);
+
+
+
+        //remove - and + buttons
+        map.setBuiltInZoomControls(false);
+        //use touch gestures to zoom
+        map.setMultiTouchControls(true);
+
+
+        updateTouchPosition();
+
+
         return root;
+    }
+
+    private void updateTouchPosition(){
+
+        MapEventsReceiver mReceive = new MapEventsReceiver() {
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint p) {
+                GeoPoint geoPoint = new GeoPoint(p.getLatitude(),p.getLongitude());
+                Marker startMarker = new Marker(map);
+                startMarker.setPosition(geoPoint);
+                startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                map.getOverlays().add(startMarker);
+                map.invalidate();
+
+                return false;
+            }
+
+            @Override
+            public boolean longPressHelper(GeoPoint p) {
+                return false;
+            }
+        };
+
+
+        MapEventsOverlay OverlayEvents = new MapEventsOverlay(getActivity(), mReceive);
+        map.getOverlays().add(OverlayEvents);
     }
 
     @Override
@@ -43,5 +108,17 @@ public class MapFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
     }
 }
