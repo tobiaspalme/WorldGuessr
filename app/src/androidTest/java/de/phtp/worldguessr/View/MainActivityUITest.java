90f5d161bb290d28bb98868f8f23408c5988ec77
Tobias <germanturboswitch@gmail.com>
2022-11-07 13:19:48 +0100

    added empty test

---
+++ app/src/androidTest/java/de/phtp/worldguessr/View/MainActivityUITest.java
@@ -0,0 +1,17 @@
+package de.phtp.worldguessr.View;
+
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Before;
+
+import de.phtp.worldguessr.view.activity.MainActivity;
+
+public class MainActivityUITest {
+
+    @Before
+    public void setUp(){
+        ActivityScenario<MainActivity> mainActivityScenario = ActivityScenario.launch(MainActivity.class);
+    }
+
+
+}
