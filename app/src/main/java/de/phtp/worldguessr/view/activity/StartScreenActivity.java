f01bdcb8eacdfce1a1275ff9d1ff5e38eff4cf17
Tobias <germanturboswitch@gmail.com>
2022-11-07 12:38:23 +0100

    implemented OnClick interface

--- app/src/main/java/de/phtp/worldguessr/view/activity/StartScreenActivity.java
+++ app/src/main/java/de/phtp/worldguessr/view/activity/StartScreenActivity.java
@@ -2,6 +2,7 @@ package de.phtp.worldguessr.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,9 +11,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import de.phtp.worldguessr.R;
 
-public class StartScreenActivity extends AppCompatActivity {
+public class StartScreenActivity extends AppCompatActivity implements Button.OnClickListener {
 
     Button startButton;
+    Button historyButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,17 +22,26 @@ public class StartScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start_screen);
 
         startButton = findViewById(R.id.button);
+        historyButton = findViewById(R.id.button2);
 
-        registerStartButton();
+        startButton.setOnClickListener(this);
+        historyButton.setOnClickListener(this);
+
     }
 
-    public void registerStartButton() {
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
                 Intent myIntent = new Intent(StartScreenActivity.this, GameScreenActivity.class);
                 startActivity(myIntent);
-            }
-        });
+                Log.d("StartClick","button pressed");
+                break;
+            case R.id.button2:
+                Intent myIntent2 = new Intent(StartScreenActivity.this, HistoryActivity.class);
+                startActivity(myIntent2);
+                Log.d("StartClick","button2 pressed");
+                break;
+        }
     }
 }
