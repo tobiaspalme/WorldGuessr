a9796bfaa9d44e936c68f6c784b5dc5175d5a566
Tobias <germanturboswitch@gmail.com>
2022-11-07 12:21:35 +0100

    added HistoryActivity.java

---
+++ app/src/main/java/de/phtp/worldguessr/view/activity/HistoryActivity.java
@@ -0,0 +1,15 @@
+package de.phtp.worldguessr.view.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import de.phtp.worldguessr.R;
+
+public class HistoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_game_history_screen);
+        super.onCreate(savedInstanceState);
+    }
+}
