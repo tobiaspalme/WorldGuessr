8697cb3377898c0e8c7ce2c55bb933e4df0b499f
Berntklug <dragon10967@googlemail.com>
2022-11-12 19:58:07 +0100

    changed name and added scores table

--- app/src/main/java/de/phtp/worldguessr/model/AppDB.java
+++ app/src/main/java/de/phtp/worldguessr/model/AppDB.java
@@ -3,7 +3,8 @@ package de.phtp.worldguessr.model;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {PictureAndPlace.class}, version = 1)
-public abstract class PictureAndPlaceDB extends RoomDatabase {
+@Database(entities = {PictureAndPlace.class, Scores.class}, version = 1)
+public abstract class AppDB extends RoomDatabase {
     public abstract PictureAndPlaceDAO pictureAndPlaceDAO();
+    public abstract ScoreDAO scoreDAO();
 }
