7d9732749e18c5a7580c11470914fd1f5e639a54
Berntklug <dragon10967@googlemail.com>
2022-11-21 13:24:52 +0100

    added test for GameControl

---
+++ app/src/androidTest/java/de/phtp/worldguessr/View/gameControlInstrumentedTest.java
@@ -0,0 +1,39 @@
+package de.phtp.worldguessr.View;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.phtp.worldguessr.R;
+import de.phtp.worldguessr.control.GameControl;
+import de.phtp.worldguessr.model.AppDB;
+import de.phtp.worldguessr.model.DAO;
+import de.phtp.worldguessr.model.PictureAndPlace;
+
+public class gameControlInstrumentedTest {
+    private AppDB db;
+    private DAO dao;
+
+    @Before
+    public void createDatabase() {
+        Context context = ApplicationProvider.getApplicationContext();
+        db = Room.inMemoryDatabaseBuilder(context, AppDB.class).build();
+        dao = db.dao();
+    }
+
+    @Test
+    public void testGetPictureId() {
+        PictureAndPlace testPicture = new PictureAndPlace();
+        testPicture.id = 0;
+        testPicture.pictureName = "atomium";
+        dao.insertPictureAndPlace(testPicture);
+        GameControl test = GameControl.getInstance(db);
+        assertEquals(R.drawable.atomium, test.getPictureId());
+    }
+}
