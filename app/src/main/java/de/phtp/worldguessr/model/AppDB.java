35c42a4aafcba83c2aa6eb891ed0ae82e90efe55
Berntklug <dragon10967@googlemail.com>
2022-11-24 02:23:24 +0100

    changed populateInitialData to populateData

--- app/src/main/java/de/phtp/worldguessr/model/AppDB.java
+++ app/src/main/java/de/phtp/worldguessr/model/AppDB.java
@@ -3,7 +3,6 @@ package de.phtp.worldguessr.model;
 import android.content.Context;
 import android.os.AsyncTask;
 
-import androidx.room.Dao;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
@@ -19,31 +18,29 @@ public abstract class AppDB extends RoomDatabase {
             sInstance = Room
                     .databaseBuilder(context.getApplicationContext(), AppDB.class, "AppDatabase")
                     .build();
-            AsyncTask.execute(() -> {sInstance.populateInitialData();});
+            AsyncTask.execute(() -> {sInstance.populateData();});
         }
         return sInstance;
     }
 
-    private void populateInitialData() {
-        if (dao().getNumberOfIds() == 0) {
-            runInTransaction(new Runnable() {
-                @Override
-                public void run() {
-                    System.out.println("TEST");
-                    PictureAndPlace place1 = new PictureAndPlace();
-                    place1.id = 0;
-                    place1.latitude = 50.896669;
-                    place1.longitude = 4.339904;
-                    place1.pictureName = "atomium";
-                    dao().insertPictureAndPlace(place1);
-                    PictureAndPlace place2 = new PictureAndPlace();
-                    place2.id = 1;
-                    place2.latitude = 43.723043;
-                    place2.longitude = 10.396985;
-                    place2.pictureName = "schiefer_turm";
-                    dao().insertPictureAndPlace(place2);
+    private void populateData() {
+        runInTransaction(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("TEST");
+                PictureAndPlace place1 = new PictureAndPlace();
+                place1.id = 0;
+                place1.latitude = 50.896669;
+                place1.longitude = 4.339904;
+                place1.pictureName = "atomium";
+                dao().insertPictureAndPlace(place1);
+                PictureAndPlace place2 = new PictureAndPlace();
+                place2.id = 1;
+                place2.latitude = 43.723043;
+                place2.longitude = 10.396985;
+                place2.pictureName = "schiefer_turm";
+                dao().insertPictureAndPlace(place2);
                 }
-            });
-        }
+        });
     }
 }
