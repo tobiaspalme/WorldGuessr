b50a56e7eb34fda41a370779835d2955876e62f6
Berntklug <dragon10967@googlemail.com>
2022-11-28 13:58:09 +0100

    added insert of scores

--- app/src/main/java/de/phtp/worldguessr/view/activity/HistoryActivity.java
+++ app/src/main/java/de/phtp/worldguessr/view/activity/HistoryActivity.java
@@ -1,54 +1,33 @@
 package de.phtp.worldguessr.view.activity;
 
-import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import de.phtp.worldguessr.R;
+import de.phtp.worldguessr.control.HistoryControl;
 
 public class HistoryActivity extends AppCompatActivity {
-
-    String[] testArray = {
-            "hallo",
-            "das",
-            "ist",
-            "ein",
-            "Test",
-            "hallo",
-            "das",
-            "ist",
-            "ein",
-            "Test",
-            "hallo",
-            "das",
-            "ist",
-            "ein",
-            "Test",
-            "hallo",
-            "das",
-            "ist",
-            "ein",
-            "Test",
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_game_history_screen);
         super.onCreate(savedInstanceState);
 
-        ArrayAdapter<String> myArrayAdapter =
-                new ArrayAdapter<String>(
+        AsyncTask.execute(() -> {
+            ListAdapter myListAdapter =
+                    new ArrayAdapter<String>(
                         this, // Die aktuelle Umgebung (diese Activity)
                         R.layout.my_list, // Die ID des Zeilenlayouts (der XML-Layout Datei)
                         R.id.my_list_text_view,   // Die ID eines TextView-Elements im Zeilenlayout
-                        testArray);  // Beispieldaten in einer ArrayList
+                        HistoryControl.getScoreList(getApplicationContext()));  // Beispieldaten in einer ArrayList
 
-        ListView myListView = (ListView) findViewById(R.id.list_view);
-        myListView.setAdapter(myArrayAdapter);
-
+            ListView myListView = (ListView) findViewById(R.id.list_view);
+            myListView.setAdapter(myListAdapter);
+        });
     }
 }
