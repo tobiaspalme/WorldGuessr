96e3909872917420914df8f475f50366aaff2c29
Tobias <germanturboswitch@gmail.com>
2022-11-28 12:38:35 +0100

    added constructor

--- app/src/main/java/de/phtp/worldguessr/model/PictureAndPlace.java
+++ app/src/main/java/de/phtp/worldguessr/model/PictureAndPlace.java
@@ -1,6 +1,7 @@
 package de.phtp.worldguessr.model;
 
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity
@@ -11,4 +12,16 @@ public class PictureAndPlace {
     public double latitude;
     public double longitude;
     public String pictureName;
+
+    @Ignore
+    public PictureAndPlace(int id) {
+        this.id = id;
+    }
+
+    public PictureAndPlace(int id, double latitude, double longitude, String pictureName) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.pictureName = pictureName;
+    }
 }
