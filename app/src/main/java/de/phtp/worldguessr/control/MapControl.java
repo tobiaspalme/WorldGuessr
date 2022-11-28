8270b1deda1020360a80c2379228461835e0fb34
Berntklug <dragon10967@googlemail.com>
2022-11-28 14:31:00 +0100

    added setFinalMarker

--- app/src/main/java/de/phtp/worldguessr/control/MapControl.java
+++ app/src/main/java/de/phtp/worldguessr/control/MapControl.java
@@ -1,8 +1,12 @@
 package de.phtp.worldguessr.control;
 
+import android.graphics.drawable.Drawable;
+
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
+
+import de.phtp.worldguessr.R;
 
 public class MapControl {
     public static void setMarker(MapView map, GeoPoint p) {
@@ -12,6 +16,14 @@ public class MapControl {
 
         Marker startMarker = new Marker(map);
         startMarker.setPosition(p);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(startMarker);
+    }
+
+    public static void setFinalMarker(MapView map, GeoPoint p, Drawable d) {
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(p);
+        startMarker.setIcon(d);
         startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         map.getOverlays().add(startMarker);
     }
