6daa85e253031501bdf2f151d6f896b8766efef3
Berntklug <dragon10967@googlemail.com>
2022-11-28 13:58:34 +0100

    added toString

--- app/src/main/java/de/phtp/worldguessr/model/Score.java
+++ app/src/main/java/de/phtp/worldguessr/model/Score.java
@@ -3,4 +3,9 @@ package de.phtp.worldguessr.model;
 public class Score {
     public String dateTime;
     public double score;
+
+    @Override
+    public String toString() {
+        return dateTime + "\t - " + score;
+    }
 }
