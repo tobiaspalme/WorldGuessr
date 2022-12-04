22b643b925985e5531696772c55714d661fb145a
Berntklug <dragon10967@googlemail.com>
2022-12-04 19:34:07 +0100

    added testRound

--- app/src/test/java/de/phtp/worldguessr/control/GameControlTest.java
+++ app/src/test/java/de/phtp/worldguessr/control/GameControlTest.java
@@ -4,17 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class DistanceCalculatorTest {
+public class GameControlTest {
     @Test
     public void testCalculateDistance() {
         double test = GameControl.calculateDistance(52.486564, 13.424146, 53.073498, 8.806185);
         assertEquals(317000, test, 500); //Berlin - Bremen
+
+        double test2 = GameControl.calculateDistance(52.486564, 13.424146, 37.798464, -122.447374);
+        assertEquals(9107740, test2, 500); //Berlin - San Francisco
     }
 
     @Test
-    public void testCalculateDistance2() {
-        double test = GameControl.calculateDistance(52.486564, 13.424146, 37.798464, -122.447374);
-        assertEquals(9107740, test, 500); //Berlin - San Francisco
+    public void testRound() {
+        double test = Math.PI;
+        assertEquals(3.14, GameControl.round(test), 0.001);
     }
-
 }
