6ac6cfe63c696d9247c5ae4bde48dc0069d57ab2
GermanTurbo <tobiaspalme2003@gmail.com>
2022-12-08 14:44:56 +0100

    removed redundant code

--- app/src/main/java/de/phtp/worldguessr/view/fragment/MapFragment.java
+++ app/src/main/java/de/phtp/worldguessr/view/fragment/MapFragment.java
@@ -128,32 +128,4 @@ public class MapFragment extends Fragment implements View.OnClickListener {
         }
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @androidx.annotation.Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        map.onPause();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        map.onResume();
-    }
-
 }
