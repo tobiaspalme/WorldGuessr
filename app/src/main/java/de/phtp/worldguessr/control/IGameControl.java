938f2b6c42199d2fae723778406671fdc47e3b6c
Tobias <germanturboswitch@gmail.com>
2022-12-12 12:48:27 +0100

    removed import

--- app/src/main/java/de/phtp/worldguessr/control/IGameControl.java
+++ app/src/main/java/de/phtp/worldguessr/control/IGameControl.java
@@ -2,13 +2,11 @@ package de.phtp.worldguessr.control;
 
 import org.osmdroid.views.MapView;
 
-import java.time.LocalDateTime;
-
 public interface IGameControl {
 
     int getPictureId();
 
-    String finalizeGame(MapView map,IMapControl mapControl);
+    String finalizeGame(MapView map, IMapControl mapControl);
 
 
 }
