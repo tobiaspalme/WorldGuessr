29418cdd78ae62da4fef27aac331c5a6e4fe4e3b
Tobias <germanturboswitch@gmail.com>
2022-12-12 12:47:39 +0100

    added comments

--- app/src/main/java/de/phtp/worldguessr/control/IMapControl.java
+++ app/src/main/java/de/phtp/worldguessr/control/IMapControl.java
@@ -2,11 +2,27 @@ package de.phtp.worldguessr.control;
 
 import org.osmdroid.util.GeoPoint;
 
+/**
+ * MapControl interface
+ */
 public interface IMapControl {
 
+    /**
+     * Sets a Geopoint as a Marker on the map
+     * @param p Geopoint
+     */
     void setMarker(GeoPoint p);
 
+    /**
+     * Sets the final Geopoint as a Marker on the map
+     * @param p Geopoint
+     */
     void setFinalMarker(GeoPoint p);
 
+    /**
+     * Draws a Polyline between two Geopoints
+     * @param start Geopoint
+     * @param finish Geopoint
+     */
     void drawLine(GeoPoint start, GeoPoint finish);
 }
