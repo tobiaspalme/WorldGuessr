8919b91a92d4f276e5126951bfcf8f0b8a3ec0e7
Berntklug <dragon10967@googlemail.com>
2023-01-16 13:07:39 +0100

    removed imports

--- app/src/main/java/de/phtp/worldguessr/model/Score.java
+++ app/src/main/java/de/phtp/worldguessr/model/Score.java
@@ -1,11 +1,5 @@
 package de.phtp.worldguessr.model;
 
-import android.annotation.SuppressLint;
-
-import java.math.BigDecimal;
-
-import de.phtp.worldguessr.control.GameControl;
-
 public class Score {
     public String dateTime;
     public double score;
