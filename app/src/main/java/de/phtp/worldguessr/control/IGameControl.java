432e6c06fa1651fb3b758c169291a8d541bae865
Berntklug <dragon10967@googlemail.com>
2023-01-16 13:54:40 +0100

    added comments

--- app/src/main/java/de/phtp/worldguessr/control/IGameControl.java
+++ app/src/main/java/de/phtp/worldguessr/control/IGameControl.java
@@ -6,8 +6,18 @@ import de.phtp.worldguessr.view.fragment.MapFragment;
 
 public interface IGameControl {
 
+    /**
+     * returns the picture id of the current picture
+     * @return the picture id of the current picture
+     */
     int getPictureId();
 
+    /**
+     * sets the final marker, draws the line between the points, calculate the distance and inserts it as a score into db
+     * @param map the current map
+     * @param fragment the current MapFragment
+     * @return snack-bar string
+     */
     String finalizeGame(MapView map, MapFragment fragment);
 
 
