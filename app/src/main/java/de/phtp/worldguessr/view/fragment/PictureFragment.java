d17f95dda5e14209766328c3ee78ef8e59fbfeb2
Tobias <germanturboswitch@gmail.com>
2023-02-11 17:25:31 +0100

    fetches picture from the DB

--- app/src/main/java/de/phtp/worldguessr/view/fragment/PictureFragment.java
+++ app/src/main/java/de/phtp/worldguessr/view/fragment/PictureFragment.java
@@ -2,25 +2,28 @@ package de.phtp.worldguessr.view.fragment;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import org.jetbrains.annotations.Nullable;
 
-import de.phtp.worldguessr.control.GameControl;
+import java.lang.reflect.Field;
+
+import de.phtp.worldguessr.R;
 import de.phtp.worldguessr.databinding.FragmentPictureBinding;
+import de.phtp.worldguessr.view.activity.GameScreenActivity;
 
 public class PictureFragment extends Fragment {
 
     private FragmentPictureBinding binding;
     private View root;
+
+    private GameScreenActivity gameScreenActivity;
 
     private ImageView imageView;
 
@@ -30,30 +33,28 @@ public class PictureFragment extends Fragment {
         binding = FragmentPictureBinding.inflate(inflater, container, false);
         root = binding.getRoot();
 
-        GameControl gameControl = GameControl.getInstance();
+        gameScreenActivity = (GameScreenActivity) getActivity();
 
         imageView = binding.fragmentPictureImageView;
 
-        if (gameControl != null) {
-            AsyncTask.execute(() -> imageView.setImageResource(gameControl.getPictureId()));
-        }
+        AsyncTask.execute(()->{
+            String resName = gameScreenActivity.getDao().getPictureName(gameScreenActivity.getCurrentImageId());
+            int pictureID = 0;
+            try {
+                Field idField = R.drawable.class.getDeclaredField(resName);
+                pictureID = idField.getInt(idField);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            imageView.setImageResource(pictureID);
+        });
+
         return root;
-    }
-
-
-    @Override
-    public void onViewCreated(@NonNull View view, @androidx.annotation.Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
     }
 }
