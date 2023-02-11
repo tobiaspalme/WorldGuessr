c9111675c11188d1fe246f14137202e1e7559052
Tobias <germanturboswitch@gmail.com>
2023-02-11 17:32:20 +0100

    added GameControl tests

--- app/src/test/java/de/phtp/worldguessr/control/GameControlTest.java
+++ app/src/test/java/de/phtp/worldguessr/control/GameControlTest.java
@@ -2,21 +2,50 @@ package de.phtp.worldguessr.control;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import de.phtp.worldguessr.model.DAO;
 
 public class GameControlTest {
+
+    private GameControl gameControl;
+
+    private DAO dao;
+
+    @Before
+    public void setUp(){
+        gameControl = new GameControl();
+        dao = Mockito.mock(DAO.class);
+    }
+
     @Test
     public void testCalculateDistance() {
-        double test = GameControl.calculateDistance(52.486564, 13.424146, 53.073498, 8.806185);
+        double test = gameControl.calculateDistance(52.486564, 13.424146, 53.073498, 8.806185);
         assertEquals(317000, test, 500); //Berlin - Bremen
 
-        double test2 = GameControl.calculateDistance(52.486564, 13.424146, 37.798464, -122.447374);
+        double test2 = gameControl.calculateDistance(52.486564, 13.424146, 37.798464, -122.447374);
         assertEquals(9107740, test2, 500); //Berlin - San Francisco
     }
 
     @Test
-    public void testRound() {
-        double test = Math.PI;
-        assertEquals(3.14, GameControl.round(test), 0.001);
+    public void testRound1(){
+        assertEquals(gameControl.round(4423.234234),4423.23,0);
+    }
+
+    @Test
+    public void testRound2(){
+        assertEquals(gameControl.round(75435.31643),75435.32,0);
+    }
+
+    @Test
+    public void testBuildSnackbarString1(){
+        assertEquals(gameControl.buildSnackbarString(4423.23),"distance: 4.42km");
+    }
+
+    @Test
+    public void testBuildSnackbarString2(){
+        assertEquals(gameControl.buildSnackbarString(442.63),"distance: 442.63m");
     }
 }
